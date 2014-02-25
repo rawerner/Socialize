c99c0cc6ec9b3b8c61a53bfa4b61e80fa01be23b
Rachel Werner <rachelannwerner@gmail.com>
2014-02-24 22:07:47 -0600

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,19 +4,26 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Socialize' }
+    let(:page_title) { '' }
 
-    it { should have_content('Socialize') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
@@ -28,8 +35,24 @@ describe "Static pages" do
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
   end
+
+  # it "should have the right links on the layout" do
+  #   visit root_path
+  #   click_link "About"
+  #   expect(page).to have_title(full_title('About Us'))
+  #   click_link "Help"
+  #   expect(page).to # fill in
+  #   click_link "Contact"
+  #   expect(page).to # fill in
+  #   click_link "Home"
+  #   click_link "Sign up now!"
+  #   expect(page).to # fill in
+  #   click_link "sample app"
+  #   expect(page).to # fill in
+  # end
 end
